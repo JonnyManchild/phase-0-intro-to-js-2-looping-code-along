34e1d7162478534e61142edda80c86721b336b67
Jonathan MacDuff <jonathan.macduff@outlook.com>
2023-11-21 20:39:42 -0800

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    const cards = []
+  for (let i = 0; i < names.length; i++) {cards.push
+    (`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+  }
+  debugger;
+return cards
+}
+writeCards(["Charlie", "Samip", "Ali"], "birthday")
+
+
+function countDown(x){
+  let countup = x;
+  while (countup > -1){
+    console.log(countup--)
+  }
+}
